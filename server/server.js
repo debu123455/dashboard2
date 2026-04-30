11c1fa62068014aa551ad284c14490a41bf5bb85
dev sharma <devsharma45637@gmail.com>
2026-04-30 01:36:32 -0800

    Add Vercel frontend URL to CORS origins

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ const corsOptions = {
       "http://localhost:5174",
       "http://127.0.0.1:5173",
       "http://127.0.0.1:5174",
+      "https://demo-dashboard-project-topaz.vercel.app",
       process.env.CORS_ORIGIN
     ].filter(Boolean);
     
